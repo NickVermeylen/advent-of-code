ca0f803816a47b4f2d48b86c39038e15e11215c7
Nick Vermeylen <nick.vermeylen@conundra.eu>
2020-12-07 08:20:48 +0100

    ditch recursive attempt

---
+++ src/main/kotlin/be/nck/aoc2020/Day07.kt
@@ -0,0 +1,48 @@
+package be.nck.aoc2020
+
+import be.nck.utils.Day
+import java.nio.file.Files
+import java.nio.file.Paths
+import java.util.*
+import java.util.stream.Collectors
+
+
+class Day07 : Day<List<String>> {
+    companion object {
+        @JvmStatic
+        fun main(args: Array<String>) {
+            val input: List<String> = Files.readAllLines(Paths.get("src/main/resources/2020/day07.txt"))
+            Day07().run(input)
+        }
+        val bags = mutableMapOf<String, Map<String, String>>()
+    }
+
+    override fun part1(input: List<String>): String {
+        input.forEach { line ->
+            val innerBags = mutableMapOf<String, String>()
+            val splitVals = line.trim().replace(".\n", "").replace("bags", "bag").replace(" bag", "")
+                    .split(" contain ")
+            splitVals[1].split(", ").filter { !it.contains("no other") }
+                    .forEach {
+                        innerBags[it.split(" ", limit = 2)[1]] = it.split(" ", limit = 2)[0]
+                    }
+            bags.put(splitVals[0], innerBags)
+        }
+
+        return optionsFor("shiny gold").count().toString()
+    }
+    fun optionsFor(s: String): List<String> {
+        val options = bags.filter { (_, v) ->
+            v.any { it.key == s }
+        }.map { it.key }.flatMap { optionsFor(it) }.toMutableList()
+        options.add(s)
+        return options.distinct().toList()
+    }
+
+
+
+
+    override fun part2(input: List<String>): String {
+        return "oops"
+    }
+}
